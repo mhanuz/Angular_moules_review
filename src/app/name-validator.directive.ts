2803817149f6e6c1c9d4bfb2b69d6cdac5eea3e8
Anuz <ceanuz66@gmail.com>
2023-02-09 13:29:52 +0600

    add all

---
+++ src/app/name-validator.directive.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+
+export function NameValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: boolean} | null => {
+        if(control.value.length >= 11) {
+            return {'NameNotAllowed': true}
+        }
+        return null;
+    }
+}
